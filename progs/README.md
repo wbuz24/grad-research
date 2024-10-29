c0792867d64557aaea6166b8a5462383a70bfc5e
wbuz24 <willbuziak@gmail.com>
2024-10-29 22:45:48 +0000

    jgraph test

---
+++ progs/README.md
@@ -0,0 +1,9 @@
+# Programs and Scripts
+
+# processingGraph.cpp
+
+Read stats.txt files from gem5 and process the data, output to a .jgr file
+
+# arrayflip.c
+
+Input the size of a big array, then the program will go through randomly and flip bits
--- progs/processingGraph.cpp
+++ progs/processingGraph.cpp
@@ -11,18 +11,21 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-  string fname, line, s;
+  string fname, line, s, title;
   ifstream file;
+  ofstream ofile;
   istringstream ss;
+  ostringstream os;
   vector <string> sv;
   int i, buf;
   double simt;
   long long mem;
 
-  if (argc != 2) { fprintf(stderr, "USAGE:\n./processGraph /path/To/stats.txt\n"); exit(1); }
+  if (argc != 2 && argc != 3) { fprintf(stderr, "USAGE:\n./processGraph /path/To/stats.txt <Graph Title>\n"); exit(1); }
 
-  // grab filename
+  // grab filename & graph title
   fname = argv[1];
+  if (argc == 3) title = argv[2];
 
   printf("Processing %s\n", fname.c_str());
 
@@ -52,8 +55,33 @@ int main(int argc, char** argv)
 
       if (sv[i] == "system.cpu.numCycles") printf("Simulated CPU cycles: %s\n", sv[i+1].c_str()); 
     }
-
   }
+
+  // close input file stream
+  file.close();
+
+  // Build the jgr output file
+  if (argc == 3) {
+    os << title << ".jgr";
+    s = os.str();
+  }
+  else s = "stats.jgr";
+
+  // open the output file steam
+  ofile.open(s);
+  if (!ofile) { fprintf(stderr, "output file %s failed to open\n", s.c_str()); exit(1); }
+
+  // Create a new graph
+  ofile << "newgraph\n\n";
+
+  // Create and define the xaxis parameters
+  ofile << "xaxis size 2\n  hash_labels fontsize 20\n";
+  ofile << "  hash_label at 1 : hello world\n\n";
+  ofile << "  hash_label at 2 : Sam's Homework\n\n"; // create labels instead of x axis
+  // define yaxis and create gray horizontal lines
+  ofile << "yaxis size 2 min 0 max 100\n  grid_lines grid_gray .7\n\n"; 
+
+  ofile << "legend top\n\n";
 
   return 0;
 }
