425cedf953afcae15c0947487e29f3df6d2af97b
wbuz24 <willbuziak@gmail.com>
2024-11-18 22:54:37 +0000

    prog updates

--- progs/arrayflip.c
+++ progs/arrayflip.c
@@ -6,23 +6,29 @@
 #include <stdlib.h>
 
 void main(int argc, char** argv) {
-  int size, iter, buf, i, *arr;
+  int size, iter, buf, i, count, *arr;
 
   if (argc != 2) { printf("Format:\n./arrtest <SIZE>\n"); exit(1); }
   
   if (sscanf(argv[1], "%d", &size) != 1) { printf("Invalid size, must be an integer\n"); exit(1); }
 
-  iter = size / 10000;
+  iter = size / 10;
 
-  arr = (int *) malloc(sizeof(int) * size);
+  arr = (int *) malloc(sizeof(int) * size); // create an array of ints the size of the input
   if (arr == NULL) { fprintf(stderr, "malloc(%d) failed.\n", size); exit(1); }
 
+  count = 0;
   for (i = 0; i < iter; i++) {
-    buf = i * rand() % size;
-    if (arr[buf]) arr[buf] = 0;
+    buf = (i * rand()) % (size - 1); // generate a random index
+    if (buf < 0) buf = buf * -1; // check that it is positive
+    if (arr[buf]) arr[buf] = 0; // flip the bit at the index
     else arr[buf] = 1;
+
+    count++; // count iterations
   }
 
-  arr = NULL;
+  printf("%d array accesses\n", count);
+
   free(arr);
+  arr = NULL;
 }
--- progs/processingGraph.cpp
+++ progs/processingGraph.cpp
@@ -1,8 +1,5 @@
 // Will Buziak
 // Process stats file from command line and output jgraph
-
-// jgraph -P input.jgr | ps2pdf - | magick -density 300 - -quality 100 out.jpg
-
 #include <iostream>
 #include <fstream>
 #include <cstdio>
@@ -12,60 +9,25 @@
 #include <cstring>
 using namespace std;
 
-void barGraph(vector <double> stats, string fname, ofstream& ofile)
+int main(int argc, char** argv)
 {
-  double simt, hostt, mem;
-
-  simt = stats[0];
-  hostt = stats[1];
-  mem = stats[2];
-
-  // Create another new graph
-  ofile << "newgraph\n\n";
-
-  ofile << "x_translate 4\n\n";
-
-  // Create and define the xaxis parameters
-  ofile << "xaxis size 2 min .8 max 3.3\n  hash 1 mhash 0 shash 0\n  no_auto_hash_labels\n";
-  ofile << "  hash_labels fontsize 12 font Times-Italix hjl rotate -60\n";
-  ofile << "  hash_label at 1 : Sim Time (milliseconds)\n\n";
-  ofile << "  hash_label at 2 : Host Time (seconds)\n\n";
-  ofile << "  hash_label at 3 : Host memory (MB)\n\n"; // create labels instead of x axis
-
-  // define yaxis and create gray horizontal lines
-  ofile << "yaxis size 2 min 0 max 1\n  grid_lines grid_gray .7\n\n"; 
-
-  // When using the gray lines you have to redraw the x axis
-  ofile << "newline pts 0.8 0 3.2 0\n\n";
-
-  // Yellow bars
-  ofile << "newcurve marktype xbar cfill 0 1 .6\n\n";
-
-  ofile << "marksize .1 .08\n\n";
-
-  ofile << "label : " << fname << "\n";
-
-  ofile << "pts\n" << "  1 " << simt << " 2 " << mem;
-
-  ofile << "\n\n";
-}
-
-vector <double> extractData(string filename) 
-{
-  int i;
-  double simt, mem, hostt;
+  string fname, line, s;
   ifstream file;
   istringstream ss;
-  string line, s;
   vector <string> sv;
-  vector <double> res;
+  int i;
+
+  if (argc != 2) { fprintf(stderr, "USAGE:\n./processGraph /path/To/stats.txt\n"); exit(1); }
+
+  // grab filename
+  fname = argv[1];
+
+  printf("Processing %s\n", fname.c_str());
 
   // open file
-  file.open(filename);
-  if (!file) { fprintf(stderr, "%s failed to open\n", filename.c_str()); exit(1); }
-
-  printf("\nProcessing %s\n\n", filename.c_str());
-
+  file.open(fname);
+  if (!file) { fprintf(stderr, "%s failed to open\n", fname.c_str()); exit(1); }
+  
   // Process each line
   while (getline(file, line)) {
     sv.clear(); // clear vector/streams and insert new line
@@ -75,82 +37,9 @@ vector <double> extractData(string filename)
     while (ss >> s) sv.push_back(s); // process string stream into vector
 
     // print each line
-    for (i = 0; i < sv.size(); i++) { 
-      if (sv[i] == "simSeconds") {
-        sscanf(sv[i+1].c_str(), "%lf", &simt); 
-        simt = simt * 1000;
-        printf("Sim time: %.2lf (milliseconds)\n", simt); 
-      }
-      if (sv[i] == "hostSeconds") {
-        sscanf(sv[i+1].c_str(), "%lf", &hostt);
-        printf("Host time: %lf (seconds)\n", hostt); 
-      }
-      if (sv[i] == "hostMemory") {
-        sscanf(sv[i+1].c_str(), "%lf", &mem);
-        mem = mem / 1000000;
-        printf("Host memory : %lf (MB)\n", mem); 
-      }
-
-      if (sv[i] == "system.cpu.numCycles") printf("Simulated CPU cycles: %s\n", sv[i+1].c_str()); 
-    }
+    for (i = 0; i < sv.size(); i++) { printf("%s ", sv[i].c_str()); }
+    printf("\n");
   }
 
-  // close input file stream
-  fflush(stdout);
-  file.close();
-  res.push_back(simt);
-  res.push_back(hostt);
-  res.push_back(mem);
-
-  return res;
-}
-
-int main(int argc, char** argv)
-{
-  string fname, f2name, str, title;
-  ofstream ofile;
-  ostringstream os;
-  double simt, mem, hostt;
-  vector <double> stats;
-
-  if (argc != 2 && argc != 3 && argc != 4) { fprintf(stderr, "USAGE:\n./processGraph /path/To/stats.txt <Graph Title>\n"); exit(1); }
-
-  // grab filename & graph title
-  fname = argv[1];
-
-  stats = extractData(fname);
-
-  simt = stats[0];
-  hostt = stats[1];
-  mem = stats[2];
-
-  // Build the jgr output file
-  if (argc == 4) {
-    os << argv[3];
-    str = os.str();
-  }
-  else if (argc == 3) str = "Compare.jgr";
-  else str = "stats.jgr";
-
-  // open the output file steam
-  ofile.open(str);
-  if (!ofile.is_open()) { fprintf(stderr, "%s has failed to open\n", str.c_str()); exit(1); }
-
-  barGraph(stats, f2name, ofile);
-
-  // If there are two graphs supplied, put them on the same page
-
-  if (argc == 4) {
-    stats.clear();
-
-    f2name = argv[2];
-    stats = extractData(f2name);
-
-    barGraph(stats, f2name, ofile);
-  }
-
-  ofile.close();
-
-  printf("\nWriting to: %s\n", str.c_str());
   return 0;
 }
