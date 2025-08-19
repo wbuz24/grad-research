39ed33ecae6c3ff02ac4048506f148eab5bda6e9
wbuz24 <willbuziak@gmail.com>
2025-08-18 18:05:36 -0600

    memset

--- resources/progs/src/arrayflip.c
+++ resources/progs/src/arrayflip.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
   int size, iter, buf, i, count, *arr;
@@ -14,12 +15,14 @@ int main(int argc, char** argv) {
 
   iter = size - 1;
 
-  arr = (int *) malloc(sizeof(int) * size); // create an array of ints the size of the input
+  arr = (int *) malloc(sizeof(int) * 2000000000); // create an array of ints the size of 2GB 
   if (arr == NULL) { fprintf(stderr, "malloc(%d) failed.\n", size); exit(1); }
+
+  memset(arr, '0', 2000000000);
 
   count = 0;
   for (i = 0; i < iter; i++) {
-    buf = (i * rand()) % (size - 1); // generate a random index
+    buf = (i * rand()) % (2000000000 - 1); // generate a random index
     if (buf < 0) buf = buf * -1; // check that it is positive
     if (arr[buf]) arr[buf] = 0; // flip the bit at the index
     else arr[buf] = 1;
