ef354a39393480927473b5f5ecb7db41efaa9098
wbuz24 <willbuziak@gmail.com>
2025-08-28 15:27:38 +0000

    checkpoints added

--- resources/progs/src/arrayflip.c
+++ resources/progs/src/arrayflip.c
@@ -5,31 +5,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define BIGARRAY 2000000000
-
-int arr[BIGARRAY];
+#include <gem5/m5ops.h>
 
 int main(int argc, char** argv) {
-  int size, buf, i, count;
+  unsigned long int size, iters, buf, i, count;
+  int *arr;
 
-  if (argc != 2) { printf("Format:\n./arrtest <SIZE>\n"); exit(1); }
+  if (argc != 3) { printf("Format:\n./arrtest <SIZE> <ITERATIONS>\n"); exit(1); }
   
-  if (sscanf(argv[1], "%d", &size) != 1) { printf("Invalid size, must be an integer\n"); exit(1); }
+  if (sscanf(argv[1], "%ld", &size) != 1) { printf("Invalid size, must be an integer\n"); exit(1); }
+  if (sscanf(argv[1], "%ld", &iters) != 1) { printf("Invalid number of iterations, must be an integer\n"); exit(1); }
 
-  memset(arr, '0', sizeof(arr));
+
+  arr = (int *) malloc(sizeof(int) * size);
+  memset(arr, 0, size);
+
+  printf("Checkpoint: Switching Processors\n");
+
+  #ifdef GEM5
+    // gem5 exit for checkpoint
+    m5_exit(0);
+  #endif // GEM5
 
   count = 0;
-  for (i = 0; i < size; i++) {
-    buf = (i * rand()) % (BIGARRAY - 1); // generate a random index
-    if (buf < 0) buf = buf * -1; // check that it is positive
+  for (i = 0; i < iters; i++) {
+    buf = (i * rand()) % (size - 1); // generate a random index
     if (arr[buf]) arr[buf] = 0; // flip the bit at the index
     else arr[buf] = 1;
 
     count++; // count iterations
   }
 
-  printf("%d array accesses\n", count);
+  printf("%ld array accesses\n", count);
+
+  #ifdef GEM5
+    m5_exit(0);
+  #endif // GEM5
 
   return 0;
 }
