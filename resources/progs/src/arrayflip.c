3f4a982e92edadb1e72ede35b6a96d6e1b4ae52f
wbuz24 <willbuziak@gmail.com>
2025-09-03 16:34:32 +0000

    unlikely m5ops

--- resources/progs/src/arrayflip.c
+++ resources/progs/src/arrayflip.c
@@ -16,14 +16,15 @@ int main(int argc, char** argv) {
   if (sscanf(argv[1], "%ld", &size) != 1) { printf("Invalid size, must be an integer\n"); exit(1); }
   if (sscanf(argv[1], "%ld", &iters) != 1) { printf("Invalid number of iterations, must be an integer\n"); exit(1); }
 
-
   arr = (int *) malloc(sizeof(int) * size);
   memset(arr, 0, size);
 
   #ifdef GEM5
     // gem5 exit for checkpoint
-    m5_exit(0);
+    m5.switchCpus();
   #endif // GEM5
+
+  printf("CPUs switched?\n");
 
   for (i = 0; i < iters; i++) {
     buf = rand() % size; // generate a random index
